3da4c9515efd4e10babc61a7d9a21dbdae04a3ab
DonCosme <charlie60686@hotmail.com>
2022-07-22 08:39:22 -0300

    Corrige denominacion de constantes
    Corrige el uso innecesario de "$" en constantes en las cuales se tomaba el valor, no el elemento. Simplifica denominacion de "listaFinal" dando directamente su valor

--- tareas/clase-5/tarea-2.js
+++ tareas/clase-5/tarea-2.js
@@ -1,15 +1,12 @@
 const $boton = document.querySelector("#boton")
 
 $boton.onclick = function hacerLista() {
-    const $primerNombre = document.querySelector("#primer-nombre").value
-    const $segundoNombre = document.querySelector("#segundo-nombre").value
-    const $apellido = document.querySelector("#apellido").value
-    const $edad = document.querySelector("#edad").value
-    let listaFinal
-
-    listaFinal = `Su primer nombre es ${$primerNombre}, su segundo nombre es ${$segundoNombre}, su apellido es ${$apellido}, y su edad es ${$edad}`
-
-    document.querySelector("h1").textContent = `Bienvenido ${$primerNombre}!`
+    const primerNombre = document.querySelector("#primer-nombre").value
+    const segundoNombre = document.querySelector("#segundo-nombre").value
+    const apellido = document.querySelector("#apellido").value
+    const edad = document.querySelector("#edad").value
+    let listaFinal = `Su primer nombre es ${primerNombre}, su segundo nombre es ${segundoNombre}, su apellido es ${apellido}, y su edad es ${edad}`
+    document.querySelector("h1").textContent = `Bienvenido ${primerNombre}!`
     document.querySelector("#lista-final").textContent = listaFinal
     return false
 }
