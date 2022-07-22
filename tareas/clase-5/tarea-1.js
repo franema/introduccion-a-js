488704dbd996d6ea842be4c15ab23e5f7c7ac664
DonCosme <charlie60686@hotmail.com>
2022-07-22 08:41:14 -0300

    Corrige denominacion de constantes
    Corrige uso innecesario de "$" en constantes en las cuales solo se tomaba el valor, no el elemento. Simplifica denominacion de salarioMensual, dando su valor directamente

--- tareas/clase-5/tarea-1.js
+++ tareas/clase-5/tarea-1.js
@@ -2,10 +2,9 @@ const $botonCalcular = document.querySelector("#boton-calcular")
 
 
 $botonCalcular.onclick = function calcularSalario() {
-  const $salarioAnual = Number(document.querySelector("#salario-anual").value)
+  const salarioAnual = Number(document.querySelector("#salario-anual").value)
   const mesesPorAnio = 12
-  let salarioMensual
-  salarioMensual = $salarioAnual / mesesPorAnio
+  let salarioMensual = salarioAnual / mesesPorAnio
   document.querySelector("#salario-mensual").value = salarioMensual
   return false
 }
